c8d23eb9079d7244a3103c87bc0a22e7b2f41b18
aum-deriv <aum@deriv.com>
2025-04-15 18:25:13 +0400

    feat: added request validation middleware ValidateRequest

--- internal/api/middlewares/validation.go
+++ internal/api/middlewares/validation.go
@@ -1,8 +1,42 @@
 package middlewares
 
 import (
+	"encoding/json"
+	"fmt"
+	"muBlog/internal/api/schemas"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/julienschmidt/httprouter"
 )
 
-func Validation(handler httprouter.Handle) httprouter.Handle {
+func ValidateRequest[schema any](res http.ResponseWriter, req *http.Request, params httprouter.Params) func(httprouter.Handle) httprouter.Handle {
+
+	// execute this wrapper func each time the endpoint is been hit
+	return func(handler httprouter.Handle) httprouter.Handle {
+
+		var body schema
+
+		json.NewDecoder(req.Body).Decode(body)
+		validate := validator.New()
+		err := validate.Struct(body)
+
+		var response schemas.ValidationErrorSchema
+		response.Code = "ValidationErrors"
+
+		if err != nil {
+			errors := err.(validator.ValidationErrors)
+			for _, err := range errors {
+				response.Errors = append(response.Errors, schemas.ErrorSchema{
+					Code:    fmt.Sprintf("Invalid%s", err.ActualTag()),
+					Message: err.Error(),
+				})
+			}
+
+			json.NewEncoder(res).Encode(response)
+			return nil
+		}
+
+		return handler
+	}
 }
--- internal/api/schemas/ErrorSchema.go
+++ internal/api/schemas/ErrorSchema.go
@@ -1,6 +1,11 @@
 package schemas
 
 type ErrorSchema struct {
-	Code	string `json:"code"`
+	Code    string `json:"code"`
 	Message string `json:"message"`
+}
+
+type ValidationErrorSchema struct {
+	Code   string        `json:"code"`
+	Errors []ErrorSchema `json:"errors"`
 }
